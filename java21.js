bdc8e6dc7e66f388428727d87e6e38bd2db3f6a3
Nicole <nicoleaugusti@yahoo.it>
2023-11-21 20:34:15 +0100

    iniziamo il lavoro

---
+++ java21.js
@@ -0,0 +1,29 @@
+
+let listaProdotti = [
+{
+    id: 1,
+    nome: "Iphone",
+    prezzo: 1000,
+},
+{
+    id: 2,
+    nome: "Mac",
+    prezzo: 3000,
+},
+
+{
+    id: 3,
+    nome: "Scrivania",
+    prezzo: 200,
+},
+{
+    id: 4,
+    nome: "Lampadari",
+    prezzo: 600,
+},
+]
+
+const nome = [ "Iphone", "Mac", "Scrivania", "Lampadari"]
+nome.sort();
+console.log(nome);
+// expected output: Arrey ["Iphone "Lampadari", "Mac", "Scrivania"]
